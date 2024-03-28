509f08ea9aada2ba1f1041705ef8c0f3d0176aa1
aloha66 <lzcbruce@live.com>
2024-03-28 15:39:39 +0800

    feat: 优化记忆（但记不住）

---
+++ ConversationSummaryBufferMemory示例.py
@@ -0,0 +1,31 @@
+# 有大概率是记不住
+from langchain_openai import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationSummaryBufferMemory
+from util import base_url
+
+
+# 创建大语言模型实例
+llm = ChatOpenAI(temperature=0.5, base_url=base_url)
+
+
+# 初始化对话链
+conversation = ConversationChain(
+    llm=llm,
+    memory=ConversationSummaryBufferMemory(llm=llm, max_token_limit=300, verbose=True),
+)
+
+
+# 第一天的对话
+# 回合1
+result = conversation.invoke("我姐姐明天要过生日，我需要一束生日花束。")
+print("回合1", result)
+# 回合2
+result = conversation.invoke("她喜欢粉色玫瑰，颜色是粉色的。")
+# print("\n第二次对话后的记忆:\n", conversation.memory.buffer)
+print("回合2", result)
+
+# 第二天的对话
+# 回合3
+result = conversation.invoke("我又来了，还记得我昨天为什么要来买花吗？")
+print("回合3", result)
