6f6c613799ffa54402468d96b796d19a62c8b4c6
aloha66 <lzcbruce@live.com>
2024-04-04 16:51:58 +0800

    fix: 补todo内容

--- 应用篇/用 Agent 查询数据库.py
+++ 应用篇/用 Agent 查询数据库.py
@@ -1,3 +1,4 @@
+# TODO 这个答案不符合预期
 from langchain_community.utilities import SQLDatabase
 from langchain_openai import ChatOpenAI
 from langchain.agents import create_sql_agent
@@ -19,8 +20,10 @@ agent_executor = create_sql_agent(
 # 使用Agent执行SQL查询
 
 questions = [
-    "哪种鲜花的存货数量最少？",
-    "平均销售价格是多少？",
+    # 这个答案不符合预期'output': 'There are X different types of flowers.'
+    "有多少种不同的鲜花？"
+    # "哪种鲜花的存货数量最少？",
+    # "平均销售价格是多少？",
 ]
 
 for question in questions:
